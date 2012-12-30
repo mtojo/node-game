b4214f180618b3e6599f8f0ef0100ede5e6a0f05
Masaki Tojo <masaki.tojo@skooler.jp>
2012-12-30 11:17:39 +0900

    first commit

---
+++ test/game.js
@@ -0,0 +1,34 @@
+var Game = require('../lib/game');
+
+describe('Game', function () {
+  it('Loop', function (done) {
+    var game = new Game({
+        framesPerSecond: 50
+      , maxFrameSkip: 5
+      , waitTime: 5
+    });
+
+    game.on('start', function() {
+      this.framesPerSecond.should.eql(50);
+      this.maxFrameSkip.should.eql(5);
+      this.waitTime.should.eql(5);
+    });
+
+    game.on('update', function() {
+      if (this.currentTick === 1) {
+        game.stop();
+      } else {
+        this.isRunning.should.be.true;
+        this.currentTick.should.eql(0);
+      }
+    });
+
+    game.on('stop', function() {
+      this.updateTicks.should.eql(1);
+
+      done();
+    });
+
+    game.start();
+  });
+});
